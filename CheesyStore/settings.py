fc062e11f0145daf51a2619a28e93683089086b7
Chris <christopherryan91@gmail.com>
2024-02-16 14:16:47 +0000

    Correct heroku host in settings.py

--- CheesyStore/settings.py
+++ CheesyStore/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cheesystore-191ae9e3f358.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['cheesystore-191ae9e3f358.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
