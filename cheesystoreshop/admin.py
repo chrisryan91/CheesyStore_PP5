7d442062a5a87e01f3b013d5333ac155559a79b9
Chris <christopherryan91@gmail.com>
2024-02-23 08:57:02 +0000

    Add cheesystoreshop models and fixtures, register models in admin

---
+++ cheesystoreshop/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Product, Category, CheeseType, Origin
+
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(CheeseType)
+admin.site.register(Origin)
