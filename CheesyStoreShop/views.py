c4322326800cae8d9553fdb2a64079d34a91afb4
Chris <christopherryan91@gmail.com>
2024-02-25 13:30:28 +0000

    Added sorting and product counts to product page

--- CheesyStoreShop/views.py
+++ CheesyStoreShop/views.py
@@ -17,7 +17,6 @@ def all_products(request):
     sort = None
     direction = None
 
-
     if request.GET:
 
         if 'sort' in request.GET:
@@ -27,6 +26,9 @@ def all_products(request):
                 if sortkey == 'name':
                     sortkey = 'lower_name'
                     products = products.annotate(lower_name=Lower('name'))
+
+                if sortkey == 'category':
+                    sortkey == 'category__name'
 
                 if 'direction' in request.GET:
                     direction = request.GET['direction']
