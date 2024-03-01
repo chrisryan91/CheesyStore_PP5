a1b7955333a0befded111b4214a93208322cddc0
Chris <christopherryan91@gmail.com>
2024-03-01 16:02:25 +0000

    Update checkout views

--- CheesyStore/settings.py
+++ CheesyStore/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary',
+    'crispy_forms',
     'cheesystoreshop',
     'home',
     'bag',
@@ -68,6 +69,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'CheesyStore.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
