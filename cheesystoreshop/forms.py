d33b027f4f684dab761c538e9876142582fc593f
Chris <christopherryan91@gmail.com>
2024-03-08 17:07:35 +0000

    Add widget to style image upload on form for adding and editing review, add html file, widgets.py and css

--- cheesystoreshop/forms.py
+++ cheesystoreshop/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 class ProductForms(forms.ModelForm):
@@ -6,6 +7,8 @@ class ProductForms(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
