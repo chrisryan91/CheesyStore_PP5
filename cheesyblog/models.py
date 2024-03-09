30f94143464c928f5c26bc286e39bb8012b8ce38
Chris <christopherryan91@gmail.com>
2024-03-09 14:49:24 +0000

    Add template, urls and migrate models for cheesyblog

--- cheesyblog/models.py
+++ cheesyblog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField("image", default="placeholder")
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments_review")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField(max_length=1000)
+    created_on = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
--- cheesyblog/views.py
+++ cheesyblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def cheesyblog(request):
+    
+    return render(request, 'cheesyblog/cheesyblog.html')
