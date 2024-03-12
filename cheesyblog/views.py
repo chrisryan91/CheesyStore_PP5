f9329bb6f587a5419932560512971675d80c3850
Chris <christopherryan91@gmail.com>
2024-03-12 09:09:18 +0000

    Add comment form for blig, update blog view template with form, update views to incorporate comment functionality

--- cheesyblog/views.py
+++ cheesyblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views import View
 from .models import Post
+from .forms import CommentForm
 
 class CheesyBlogListView(ListView):
     model = Post
@@ -24,6 +25,32 @@ class PostDetail(View):
             'cheesyblog/cheesyblogpost.html',
             {
                 "post": post,
-                "comments": comments
+                "comments": comments,
+                "comment_form": CommentForm()
+            },
+        )
+    
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "cheesyblog/cheesyblogpost.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
             },
         )
