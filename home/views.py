67854df3e4a51776703268837616a7f3001547dd
Chris <christopherryan91@gmail.com>
2024-03-16 00:13:54 +0000

    Style blog preview on index, rearrange elements on navbar, add padding to contact form

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from cheesyblog.models import Post
 
 def index(request):
 
-    blog_posts = Post.objects.filter(status=1).order_by('-created_on')[:1]
+    blog_posts = Post.objects.filter(status=1).order_by('-created_on')[:3]
     return render(request, 'home/index.html', {'blog_posts': blog_posts})
