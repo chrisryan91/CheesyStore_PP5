416d0ba0a855d9e59d3ee0a120aafd7099884c38
Chris <christopherryan91@gmail.com>
2024-03-22 21:47:00 +0000

    Add new arrival category and render new arrivals on index.html, update index views to render new_arrival category

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from cheesyblog.models import Post
+from cheesystoreshop.models import Product
 
 def index(request):
     blog_posts1 = Post.objects.filter(status=1).order_by('-created_on')[:3]
     blog_posts2 = Post.objects.filter(status=1).order_by('-created_on')[:1]
-    context = {'blog_posts1': blog_posts1, 'blog_posts2': blog_posts2}
+
+    category_name = "new_arrival"
+    new_arrival = Product.objects.filter(category__name=category_name).order_by('-id')[:1]
+
+    context = {'blog_posts1': blog_posts1,
+               'blog_posts2': blog_posts2,
+               'new_arrival': new_arrival,}
                
     return render(request, 'home/index.html', context)
