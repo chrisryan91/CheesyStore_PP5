be120b1c72d54c06085ce2b8a964b3d95c31ed37
Chris <christopherryan91@gmail.com>
2024-03-23 16:19:43 +0000

    Deployment commit - add aws setting for static files

--- CheesyStore/settings.py
+++ CheesyStore/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'profiles',
     'cheesyblog',
     'faq',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -183,6 +184,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'cheesystore'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
