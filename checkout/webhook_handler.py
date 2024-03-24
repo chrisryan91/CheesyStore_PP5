e93fd7d748a8ab63cc1bce3b2f3aaefb32e6ad32
Chris <christopherryan91@gmail.com>
2024-03-24 16:24:19 +0000

    Alter backslash to forward slash for crosscompatibility for heroku

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,11 +20,11 @@ class StripeWH_Handler:
         cust_email = order.email
         print(settings.DEFAULT_FROM_EMAIL)
         subject = render_to_string(
-            'checkout\confirmation_emails\confirmation_email_subject.txt',
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
 
         body = render_to_string(
-            'checkout\confirmation_emails\confirmation_email_body.txt',
+            'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
