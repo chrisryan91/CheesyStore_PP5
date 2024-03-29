a079f8175a55204d4c1f6c239d293e22223df4ca
Chris <christopherryan91@gmail.com>
2024-03-29 23:23:47 +0000

    Deployment commit

--- cheesyblog/templatetags/blogtags.py
+++ cheesyblog/templatetags/blogtags.py
@@ -2,7 +2,8 @@ from django import template
 
 register = template.Library()
 
+# The @register.filter decorator registers the following function as a custom template filter.
 @register.filter
 def split(value, delimiter):
-    """Splits the string on the delimiter."""
+    # Splits the string.
     return value.split(delimiter)
