6f9d5e146de97f2b6d56c304dc3dbdd71dbc10d7
Chris <christopherryan91@gmail.com>
2024-04-02 20:11:10 +0100

    Render new arrivals into template, restyle button elements

---
+++ cheesyblog/static/cheesyblog/js/blogscript.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all elements with the class 'confirmButton'
+    var confirmButtons = document.querySelectorAll('.confirmButton');
+    
+    // Iterate over the NodeList and attach an event listener to each button
+    confirmButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the form from submitting immediately
+            var userResponse = confirm('Are you sure you want to delete this comment?');
+            if (userResponse) {
+                // If the user confirms, submit the form programmatically
+                this.form.submit();
+            }
+            // If the user does not confirm, do nothing (the form will not be submitted)
+        });
+    });
+});
