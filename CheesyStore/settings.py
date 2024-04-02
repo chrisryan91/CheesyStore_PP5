71417aca0464f84835d20552ea5101b063af04dd
Chris <christopherryan91@gmail.com>
2024-04-02 20:57:35 +0100

    Deployment commit

--- CheesyStore/settings.py
+++ CheesyStore/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cheesystore-191ae9e3f358.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['cheesystore-191ae9e3f358.herokuapp.com']
 
 
 # Application definition
