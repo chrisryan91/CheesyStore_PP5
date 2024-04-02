20325332ee9a0f078c4e6cc6129209a73d2498af
Chris <christopherryan91@gmail.com>
2024-04-02 21:35:56 +0100

    Set debug to false

--- CheesyStore/settings.py
+++ CheesyStore/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cheesystore-191ae9e3f358.herokuapp.com/', '127.0.0.1']
 
